ed9072fc968f7bf5b19ae765e831c3f046574965
KrakenTim <KrakenTim@freenet.de>
2022-05-09 18:13:30 +0200

    Added fancy water

--- Assets/Scripts/CatCode/HexGridChunk.cs
+++ Assets/Scripts/CatCode/HexGridChunk.cs
@@ -95,36 +95,76 @@ public class HexGridChunk : MonoBehaviour
     void TriangulateWater(HexDirection direction, HexCell cell, Vector3 center)
     {
         center.y = cell.WaterSurfaceY;
-        Vector3 c1 = center + HexMetrics.GetFirstSolidCorner(direction);
-        Vector3 c2 = center + HexMetrics.GetSecondSolidCorner(direction);
-
-        water.AddTriangle(center, c1, c2);
-
-        if (direction <= HexDirection.SE)
+        HexCell neighbor = cell.GetNeighbor(direction);
+        if (neighbor != null && !neighbor.IsUnderwater)
         {
-            HexCell neighbor = cell.GetNeighbor(direction);
-            if (neighbor == null || !neighbor.IsUnderwater)
+            TriangulateWaterShore(direction, cell, neighbor, center);
+        }
+        else
+        {
+            TriangulateOpenWater(direction, cell, neighbor, center);
+        }
+
+        void TriangulateOpenWater(HexDirection direction, HexCell cell, HexCell neighbor, Vector3 center)
+        {
+            Vector3 c1 = center + HexMetrics.GetFirstSolidCorner(direction);
+            Vector3 c2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+            water.AddTriangle(center, c1, c2);
+
+            if (direction <= HexDirection.SE && neighbor != null)
             {
-                return;
-            }
+                //			HexCell neighbor = cell.GetNeighbor(direction);
+                //			if (neighbor == null || !neighbor.IsUnderwater) 
+                //          {
+                //			    return;
+                //		    }
 
-            Vector3 bridge = HexMetrics.GetBridge(direction);
-            Vector3 e1 = c1 + bridge;
-            Vector3 e2 = c2 + bridge;
+                Vector3 bridge = HexMetrics.GetBridge(direction);
+                Vector3 e1 = c1 + bridge;
+                Vector3 e2 = c2 + bridge;
 
-            water.AddQuad(c1, c2, e1, e2);
+                water.AddQuad(c1, c2, e1, e2);
 
-            if (direction <= HexDirection.E)
-            {
-                HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
-                if (nextNeighbor == null || !nextNeighbor.IsUnderwater)
+                if (direction <= HexDirection.E)
                 {
-                    return;
+                    HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+                    if (nextNeighbor == null || !nextNeighbor.IsUnderwater)
+                    {
+                        return;
+                    }
+                    water.AddTriangle(
+                        c2, e2, c2 + HexMetrics.GetBridge(direction.Next())
+                    );
                 }
-                water.AddTriangle(
-                    c2, e2, c2 + HexMetrics.GetBridge(direction.Next())
-                );
             }
+        }
+    }
+
+    void TriangulateWaterShore(HexDirection direction, HexCell cell, HexCell neighbor, Vector3 center)
+    {
+        EdgeVertices e1 = new EdgeVertices(center + HexMetrics.GetFirstSolidCorner(direction), center + HexMetrics.GetSecondSolidCorner(direction));
+        water.AddTriangle(center, e1.v1, e1.v2);
+        water.AddTriangle(center, e1.v2, e1.v3);
+        water.AddTriangle(center, e1.v3, e1.v4);
+        //water.AddTriangle(center, e1.v4, e1.v5); // for rivers
+
+        Vector3 bridge = HexMetrics.GetBridge(direction);
+        EdgeVertices e2 = new EdgeVertices(
+            e1.v1 + bridge,
+            e1.v4 + bridge //e1.v5 + bridge  //for rivers
+        );
+        water.AddQuad(e1.v1, e1.v2, e2.v1, e2.v2);
+        water.AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
+        water.AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
+        //water.AddQuad(e1.v4, e1.v5, e2.v4, e2.v5); rivers
+
+        HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+        if (nextNeighbor != null)
+        {
+            water.AddTriangle(
+                e1.v4, e2.v4, e1.v4 + HexMetrics.GetBridge(direction.Next()) // e1.v5 instead of e1.v4 for rivers
+            );
         }
     }
 
